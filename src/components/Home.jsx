a2e07c8abc05d9eab59acfbb37830a537a8fb67a
Vivek Negi <negivivek13@gmail.com>
2022-12-21 23:34:34 +0530

    fast

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -71,7 +71,7 @@ const MyCarousel = () => (
   <Carousel
     autoPlay
     infiniteLoop
-    interval={1000}
+    interval={3000}
     showStatus={false}
     showThumbs={false}
     showArrows={false}
